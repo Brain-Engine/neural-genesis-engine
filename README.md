81ffb4b46409452250a7aef536890fef4d89f0f6
StephenTerror <2684109034@qq.com>
2021-07-13 23:03:18 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # neural-genesis-engine
+
+## tips
+
+#### install dependence
+
+`pip install -r docs/requirements.txt`
+
+#### train model
+
+`sh scripts/run.sh`
+
+#### custom datasets 
+
+`vi core/dataset/data/__init__.py`
+
+#### custom models
+
+`vi core/models/__init__.py`
+
--- dataset_download.py
+++ dataset_download.py
@@ -2,8 +2,8 @@ from core.datasets.data_zoo import get_data_by_name
 from torchvision import datasets
 
 if __name__ == '__main__':
-    get_data_by_name("cifar10", data_dir="./data/dataset")
     get_data_by_name("mnist", data_dir="./data/dataset")
+    get_data_by_name("cifar10", data_dir="./data/dataset")
     get_data_by_name("fashion_mnist", data_dir="./data/dataset")
 
     datasets.MNIST(root="./data/dataset", download=True)
